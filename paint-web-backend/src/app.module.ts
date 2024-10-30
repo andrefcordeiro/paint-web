527c4915a3b560e3b298abeb7c7882b1eb8c76bc
André Ferreira <andrefc36@gmail.com>
2024-10-30 19:46:32 -0300

    Using dotenv for .env files

--- paint-web-backend/src/app.module.ts
+++ paint-web-backend/src/app.module.ts
@@ -4,14 +4,13 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AuthModule } from './auth/auth.module';
-import { ConfigModule } from '@nestjs/config';
+
+import * as dotenv from 'dotenv';
+dotenv.config({ path: `${__dirname}/../.env` });
 
 @Module({
   imports: [
-    ConfigModule.forRoot({
-      isGlobal: true,
-    }),
-    MongooseModule.forRoot('mongodb://localhost/paint-web'),
+    MongooseModule.forRoot(process.env.MONGO_CONNECTION_URI),
     UsersModule,
     AuthModule,
   ],
