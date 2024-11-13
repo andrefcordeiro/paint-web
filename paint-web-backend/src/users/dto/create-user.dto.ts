a24c7ce6c26f91b1bcb1db53da86424c5b0369a1
André Ferreira <andrefc36@gmail.com>
2024-11-12 21:43:03 -0300

    Adding user validation

--- paint-web-backend/src/users/dto/create-user.dto.ts
+++ paint-web-backend/src/users/dto/create-user.dto.ts
@@ -1,9 +1,15 @@
+import { IsEmail, IsNotEmpty, Length } from 'class-validator';
+
 export class CreateUserDto {
+  @IsNotEmpty()
   name: string;
 
+  @IsNotEmpty()
   username: string;
 
+  @IsEmail()
   email: string;
 
+  @Length(8)
   password: string;
 }
