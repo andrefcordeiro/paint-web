c11766beb68e4bdf2274dc9b5958d3c86e696da7
André Ferreira <andrefc36@gmail.com>
2024-12-21 22:22:31 -0300

    Including user id on the login response body

--- paint-web-backend/src/users/dto/user.dto.ts
+++ paint-web-backend/src/users/dto/user.dto.ts
@@ -1,9 +1,11 @@
 import { IsEmail, IsNotEmpty } from 'class-validator';
 import { User } from '../entities/user.entity';
+import { Types } from 'mongoose';
 
 export class UserDto {
   static fromDomain(user: User): UserDto {
     return {
+      id: user.id,
       name: user.name,
       username: user.username,
       email: user.email,
@@ -11,6 +13,8 @@ export class UserDto {
       updatedAt: user.updatedAt,
     };
   }
+
+  id: Types.ObjectId;
 
   @IsNotEmpty()
   name: string;
