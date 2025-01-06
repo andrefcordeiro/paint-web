3d654ad0d54e91bf07b344aab1bd8c9142cee9ac
André Ferreira <andrefc36@gmail.com>
2025-01-05 23:00:54 -0300

    Changing image upload endpoint to save images on S3

--- paint-web-backend/src/images/images.controller.ts
+++ paint-web-backend/src/images/images.controller.ts
@@ -35,40 +35,40 @@ export class ImagesController {
    *  Save images on S3.
    *
    */
-  // @Post()
-  // @UseGuards(AuthGuard)
-  // @UseInterceptors(FileInterceptor('image', multerConfig))
-  // async uploadImage(
-  //   @UploadedFile() image: Express.MulterS3.File,
-  //   @Req() req: Request,
-  // ) {
-  //   const user = await this.userService.findByUsername(req['user'].username);
-  //   return this.imagesService.saveImage(user.id, image);
-  // }
+  @Post()
+  @UseGuards(AuthGuard)
+  @UseInterceptors(FileInterceptor('image', multerConfig))
+  async uploadImage(
+    @UploadedFile() image: Express.MulterS3.File,
+    @Req() req: Request,
+  ) {
+    const user = await this.userService.findByUsername(req['user'].username);
+    return this.imagesService.saveImage(user.id, image);
+  }
 
-  // @Post('multiple')
-  // @UseGuards(AuthGuard)
-  // @UseInterceptors(FileFieldsInterceptor([{ name: 'images' }], multerConfig))
-  // async uploadMultipleImages(
-  //   @UploadedFiles()
-  //   images: Express.MulterS3.File[],
-  //   @Req() req: Request,
-  // ) {
-  //   const user = await this.userService.findByUsername(req['user'].username);
-  //   return this.imagesService.saveImages(user.id, images);
-  // }
+  @Post('multiple')
+  @UseGuards(AuthGuard)
+  @UseInterceptors(FileFieldsInterceptor([{ name: 'images' }], multerConfig))
+  async uploadMultipleImages(
+    @UploadedFiles()
+    images: Express.MulterS3.File[],
+    @Req() req: Request,
+  ) {
+    const user = await this.userService.findByUsername(req['user'].username);
+    return this.imagesService.saveImages(user.id, images);
+  }
 
   /**
    *  Save images locally.
    *
    */
-  @Post()
-  @UseGuards(AuthGuard)
-  @UseInterceptors(FileInterceptor('image', multerConfigLocal))
-  async uploadImage(@UploadedFile() image, @Req() req: Request) {
-    const user = await this.userService.findByUsername(req['user'].username);
-    return this.imagesService.saveImageLocally(user.id, image);
-  }
+  // @Post()
+  // @UseGuards(AuthGuard)
+  // @UseInterceptors(FileInterceptor('image', multerConfigLocal))
+  // async uploadImage(@UploadedFile() image, @Req() req: Request) {
+  //   const user = await this.userService.findByUsername(req['user'].username);
+  //   return this.imagesService.saveImageLocally(user.id, image);
+  // }
 
   @Get()
   @UseGuards(AuthGuard)
